7cd6bd0a218d6b2e5d8b7af1b069ffbad507a81e
Alex Paige <apaige161@gmail.com>
2020-12-12 15:57:26 -0500

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faSeedling, faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-garden';
+
+  //display list of veggies
+  plants: string[] = [
+    "carrot",
+    "cabbage",
+    "cucumber",
+    "hot pepper",
+    "mild pepper",
+    "garlic",
+    "basil",
+    "lettuce"
+  ]
+
+  garden: string[] = []
+
+  /********************************
+  
+    add a plant to garden when clicked on
+
+    the button in the html gets the index of the item and sends it to be added as an index number
+      from plants
+  
+  *******************************/
+  addToGarden(plantToAdd: number) {
+    this.garden.push(this.plants[plantToAdd]);
+  }
+
+
+  
+  //initialize an array with 9 places and set each item as 'empty'
+  itemArray: string[] = new Array(9).fill('empty');
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconSeedlingComponent } from './icon-seedling/icon-seedling.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconSeedlingComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
